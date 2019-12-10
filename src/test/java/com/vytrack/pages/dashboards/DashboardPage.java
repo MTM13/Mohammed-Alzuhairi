084bc835e7304edb2aa55f3cbd66851d6a100101
m.alzuhahiri@gmail.com <Ronnie1986$>
2019-12-10 06:48:47 -0500

    update

--- src/test/java/com/vytrack/pages/dashboards/DashboardPage.java
+++ src/test/java/com/vytrack/pages/dashboards/DashboardPage.java
@@ -1,8 +1,12 @@
 package com.vytrack.pages.dashboards;
 
+<<<<<<< .merge_file_a22492
 import com.vytrack.utilities.BasePage;
 import com.vytrack.utilities.Driver;
 import org.openqa.selenium.support.PageFactory;
+=======
+import com.vytrack.base.BasePage;
+>>>>>>> .merge_file_a23156
 
 public class DashboardPage extends BasePage {
     public DashboardPage() {
